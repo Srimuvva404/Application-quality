e60fdef5da55110d2fca7c96e90319136c07e1cf
Srimuvva404 <128165457+Srimuvva404@users.noreply.github.com>
2023-03-22 17:26:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Test!'
 @app.route('/number/<int:n>')
 def check_number(n):
     if n > 100:
